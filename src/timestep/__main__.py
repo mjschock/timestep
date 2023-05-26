1fdf4d8e0952836a012ff23bc30c878d938d25f5
Michael Schock <m@mjschock.com>
2023-05-25 18:23:31 -0700

    f
    Former-commit-id: f2fd3b200adfac24a76ab93304a34ea1f925f3fc

--- src/timestep/__main__.py
+++ src/timestep/__main__.py
@@ -1,21 +1,151 @@
-#!/usr/bin/env python
+import typer
+import os
+
 from constructs import Construct
-from cdktf import App, TerraformStack, CloudBackend, NamedCloudWorkspace
+from cdktf import App, TerraformDataSource, TerraformOutput, TerraformProvider, TerraformResource, TerraformStack
+from cdktf_cdktf_provider_digitalocean.provider import DigitaloceanProvider as DigitaloceanTerraformProvider
+from cdktf_cdktf_provider_digitalocean.droplet import Droplet as DigitaloceanTerraformResource
+from cdktf_cdktf_provider_digitalocean.data_digitalocean_droplet import DataDigitaloceanDroplet as DigitaloceanTerraformDataSource
+
+from omegaconf import DictConfig, OmegaConf
+import hydra
+
+from lib.imports.multipass.provider import MultipassProvider as MultipassTerraformProvider
+from lib.imports.multipass.instance import Instance as MultipassTerraformResource
+from lib.imports.multipass.data_multipass_instance import DataMultipassInstance as MultipassTerraformDataSource
 
 
-class MyStack(TerraformStack):
-    def __init__(self, scope: Construct, id: str):
+class MainTerraformStack(TerraformStack):
+    def __init__(self, scope: Construct, id: str, target: str):
         super().__init__(scope, id)
 
-        # define resources here
+        provider = self.get_provider(id, target)
+
+        resource = self.get_resource(id, target, provider)
+
+        data_source = self.get_data_source(id, target, provider, resource)
+
+        output = self.get_output(target, data_source)
+
+    def get_provider(self, id, target):
+        if target in ["localhost", "multipass"]:
+            provider = MultipassTerraformProvider(
+                id=f"{id}-{target}-provider",
+                scope=self,
+            )
+
+        elif target == "prod":
+            do_token = os.environ.get("DO_TOKEN", "")
+
+            provider = DigitaloceanTerraformProvider(
+                id=f"{id}-{target}-provider",
+                scope=self,
+                token=do_token,
+            )
+
+        else:
+            raise ValueError(f"Unknown target: {target}")
+
+        assert isinstance(provider, TerraformProvider)
+        return provider
+
+    def get_resource(self, id, target, provider):
+        if target in ["localhost", "multipass"]:
+            cwd = os.getcwd()
+            cloudinit_file = f"{cwd}/conf/base/cloud.yaml"
+
+            resource = MultipassTerraformResource(
+                cloudinit_file=cloudinit_file,
+                cpus=2,
+                disk="40G",
+                id=f"{id}-{target}-resource",
+                image="22.04", # "ros2-humble",
+                # image="ros2-humble",
+                # memory="4G",
+                name=f"{id}",
+                provider=provider,
+                scope=self,
+            )
+
+        elif target == "prod":
+            cwd = os.getcwd()
+            cloudinit_file = f"{cwd}/conf/base/cloud.yaml"
+
+            resource = DigitaloceanTerraformResource(
+                id_=f"{id}-{target}-resource",
+                image="ubuntu-22-04-x64",
+                name=f"{id}",
+                provider=provider,
+                region='sfo3',
+                scope=self,
+                size='s-1vcpu-512mb-10gb',
+                user_data=cloudinit_file,
+            )
+
+        else:
+            raise ValueError(f"Unknown target: {target}")
+
+        assert isinstance(resource, TerraformResource)
+        return resource
+
+    def get_data_source(self, id, target, provider, resource):
+        if target in ["localhost", "multipass"]:
+            data_source = MultipassTerraformDataSource(
+                id=f"{id}-{target}-data_source",
+                name=resource.name,
+                provider=provider,
+                scope=self,
+            )
+
+        elif target == "prod":
+            data_source = DigitaloceanTerraformDataSource(
+                id_=f"{id}-{target}-data_source",
+                name=resource.name,
+                provider=provider,
+                scope=self,
+            )
+
+        else:
+            raise ValueError(f"Unknown target: {target}")
+
+        assert isinstance(data_source, TerraformDataSource)
+        return data_source
+
+    def get_output(self, target, data_source):
+        if target in ["localhost", "multipass"]:
+            output = TerraformOutput(
+                id=f"{target}-ipv4",
+                value=data_source.ipv4,
+                scope=self,
+            )
+
+        elif target == "prod":
+            output = TerraformOutput(
+                id=f"{target}-ipv4",
+                value=data_source.ipv4_address,
+                scope=self,
+            )
+
+        else:
+            raise ValueError(f"Unknown target: {target}")
+
+        assert isinstance(output, TerraformOutput)
+        return output
 
 
-app = App()
-stack = MyStack(app, "timestep")
-CloudBackend(stack,
-  hostname='app.terraform.io',
-  organization='timestep-ai',
-  workspaces=NamedCloudWorkspace('timestep-ai')
-)
+@hydra.main(config_name="config", config_path="conf", version_base=None)
+def main(cfg: DictConfig) -> None:
+    app_name = cfg.target.app_name
+    env = cfg.target.env
 
-app.synth()
+    print(f"Running {app_name} app in {env} mode")
+
+    app = App()
+
+    MainTerraformStack(scope=app, id=app_name, target=env)
+
+    app.synth()
+
+
+if __name__ == "__main__":
+    main()
