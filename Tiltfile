96185fa9e98694b651ab92d3026f7f291dd8c248
Michael Schock <m@mjschock.com>
2023-06-24 14:14:53 -0700

    f
    Former-commit-id: 5e903b9b052837b23be97160a1b69a7f17f6fba7

--- Tiltfile
+++ Tiltfile
@@ -33,25 +33,32 @@ local_resource(
 
 local_resource(
     'poetry run cdktf deploy',
-    cmd='poetry run cdktf deploy --auto-approve $DOMAIN-tf-stack',
+    # cmd='poetry run cdktf deploy --auto-approve --output $CDKTF_OUTDIR $STACK_ID',
+    cmd='poetry run cdktf deploy --auto-approve $STACK_ID',
     deps=[
+        '.env',
         'src/timestep/__main__.py',
         'src/timestep/conf.py',
         'src/timestep/infra/stacks/base/stack.py',
+        'src/timestep/infra/stacks/base/constructs/cloud_init_config/construct.py',
+        'src/timestep/infra/stacks/base/constructs/cloud_instance/construct.py',
     ],
     env={
-        "DOMAIN": os.getenv('DOMAIN', 'timestep.local'),
+        # "CDKTF_OUTDIR": os.getenv('CDKTF_OUTDIR'),
+        "STACK_ID": os.getenv('STACK_ID'),
     },
     labels=['deploy'],
     resource_deps=['poetry install']
 )
 
 cmd_button('poetry run cdktf destroy',
-    argv=['sh', '-c', 'poetry run cdktf destroy $AUTO_APPROVE $DOMAIN-tf-stack'],
+    # argv=['sh', '-c', 'poetry run cdktf destroy $AUTO_APPROVE --output $CDKTF_OUTDIR $STACK_ID'],
+    argv=['sh', '-c', 'poetry run cdktf destroy $AUTO_APPROVE $STACK_ID'],
     icon_name='delete',
     inputs=[
         bool_input('AUTO_APPROVE', true_string='--auto-approve', false_string='', default=True),
-        text_input('DOMAIN', default=os.getenv('DOMAIN', 'timestep.local')),
+        # text_input('CDKTF_OUTDIR', default=os.getenv('CDKTF_OUTDIR')),
+        text_input('STACK_ID', default=os.getenv('STACK_ID')),
     ],
     resource='poetry run cdktf deploy',
     text='poetry run cdktf destroy',
@@ -59,8 +66,12 @@ cmd_button('poetry run cdktf destroy',
 
 local_resource(
     'hostctl watch',
-    serve_cmd='cat dist/.etchosts | sudo $(which hostctl) add timestep-ai --wait 0',
+    serve_cmd='cat $HOSTS_FILE_PATH | sudo $(which hostctl) add $STACK_ID --wait 0',
     deps=[
-        'dist/.etchosts',
+        '$HOSTS_FILE_PATH',
     ],
+    env={
+        "HOSTS_FILE_PATH": os.getenv('HOSTS_FILE_PATH'),
+        "STACK_ID": os.getenv('STACK_ID'),
+    },
 )
--- src/timestep/conf.py
+++ src/timestep/conf.py
@@ -1,36 +1,57 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 import pathlib
 import os
 
 BASE_PATH = pathlib.Path.cwd()
 DIST_PATH: str = f"{BASE_PATH}/dist"
 
-class AppConfig(BaseModel): # TODO: EnvConfig instead? AppConfig is a Terraform concept
-    CADDY_INGRESS_CONTROLLER_EMAIL: str = "m@mjschock.com"
-    CADDY_INGRESS_CONTROLLER_CHART_PATH: str = f"{BASE_PATH}/src/timestep/infra/ingress/charts/caddy-ingress-controller"
-    DOCKER_REGISTRY_HTPASSWD: str = "admin:$2a$10$70qctd/h6OkNr4ugiFoPWuBLowf5TqRr/Y3R.6LGPCqelHG5JRa7e" # admin:BVNt6uwp95H6o4e59022
-    CDKTF_OUTDIR: str = os.environ.get("CDKTF_OUTDIR", f"{DIST_PATH}/cdktf.out")
-    CLOUD_CONFIG_PATH: str = f"{DIST_PATH}/cloud-config.yml"
-    CLOUD_INSTANCE_PROVIDER: str = "multipass"
+CPUS: int = 1
+DISK_SIZE_GB: int = 10
+
+class MainConfig(BaseModel):
+    class CLOUD_INSTANCE_PROVIDERS:
+        MULTIPASS: str = "multipass"
+        DIGITALOCEAN: str = "digitalocean"
+
+    # CADDY_INGRESS_CONTROLLER_EMAIL: str = "m@mjschock.com"
+    # CADDY_INGRESS_CONTROLLER_CHART_PATH: str = f"{BASE_PATH}/src/timestep/infra/ingress/charts/caddy-ingress-controller"
+    # DOCKER_REGISTRY_HTPASSWD: str = "admin:$2a$10$70qctd/h6OkNr4ugiFoPWuBLowf5TqRr/Y3R.6LGPCqelHG5JRa7e" # admin:BVNt6uwp95H6o4e59022
+    # CDKTF_OUTDIR: str = os.environ.get("CDKTF_OUTDIR", f"{DIST_PATH}/cdktf.out")
+    # CDKTF_OUTDIR: str = None
+    # CLOUD_CONFIG_PATH: str = f"{DIST_PATH}/cloud-config.yml"
+    # CLOUD_CONFIG_PATH: str = f"{DIST_PATH}/cloud-config.yaml"
+    CLOUD_CONFIG_PATH: str = "cloud-config.yaml"
+    CLOUD_INSTANCE_PROVIDER: str = CLOUD_INSTANCE_PROVIDERS.MULTIPASS
+    # CLOUD_INSTANCE_PROVIDER: str = "multipass"
     CLOUD_INSTANCE_NAME: str = "timestep-ai"
-    CPUS: int = 1
-    DISK_SIZE_GB: int = 10
-    DO_DROPLET_IMAGE: str = "ubuntu-22-04-x64"
-    DO_DROPLET_REGION: str = "sfo3"
-    DO_DROPLET_SIZE: str = f"s-{CPUS}vcpu-512mb-{DISK_SIZE_GB}gb"
-    DO_TOKEN: str = os.environ.get("DO_TOKEN")
-    DOMAIN: str = "timestep.local"
-    HOSTS_FILE_PATH: str = f"{DIST_PATH}/.etchosts"
-    KUBE_CONFIG_PATH: str = f"{DIST_PATH}/kube-config.yml"
-    KUBE_CONTEXT: str = "timestep-ai"
+    # DO_DROPLET_IMAGE: str = "ubuntu-22-04-x64"
+    # DO_DROPLET_REGION: str = "sfo3"
+    # DO_DROPLET_SIZE: str = f"s-{CPUS}vcpu-512mb-{DISK_SIZE_GB}gb"
+    # DO_TOKEN: str = os.environ.get("DO_TOKEN")
+    # DOMAIN: str = "timestep.local"
+    # HOSTS_FILE_PATH: str = f"{DIST_PATH}/.etchosts"
+    # KUBE_CONFIG_PATH: str = f"{DIST_PATH}/kube-config.yml"
+    # KUBE_CONTEXT: str = "timestep-ai"
+    KUBECONTEXT: str = "default"
     MULTIPASS_INSTANCE_CPUS: int = CPUS
     MULTIPASS_INSTANCE_DISK: str = f"{DISK_SIZE_GB}G"
     MULTIPASS_INSTANCE_IMAGE: str = "22.04"
-    NAMECHEAP_API_KEY: str = os.environ.get("NAMECHEAP_API_KEY")
-    NAMECHEAP_API_USER: str = os.environ.get("NAMECHEAP_API_USER")
-    NAMECHEAP_USER_NAME: str = os.environ.get("NAMECHEAP_USER_NAME")
-    PLATFORM_CHART_PATH: str = f"{DIST_PATH}/charts/platform"
-    REGISTRY_URL=f"registry.{DOMAIN}"
-    SSH_AUTHORIZED_KEYS_PATH: str = f"{DIST_PATH}/.ssh/authorized_keys"
-    SSH_PUBLIC_KEY_PATH: str = f"{DIST_PATH}/.ssh/id_rsa.pub"
-    SSH_PRIVATE_KEY_PATH: str = f"{DIST_PATH}/.ssh/id_rsa"
+    # NAMECHEAP_API_KEY: str = os.environ.get("NAMECHEAP_API_KEY")
+    # NAMECHEAP_API_USER: str = os.environ.get("NAMECHEAP_API_USER")
+    # NAMECHEAP_USER_NAME: str = os.environ.get("NAMECHEAP_USER_NAME")
+    # PLATFORM_CHART_PATH: str = f"{DIST_PATH}/charts/platform"
+    # REGISTRY_URL=f"registry.{DOMAIN}"
+    # SSH_AUTHORIZED_KEYS_PATH: str = f"{DIST_PATH}/.ssh/authorized_keys"
+    SSH_PUBLIC_KEY: str = None
+    # SSH_PUBLIC_KEY_PATH: str = f"{DIST_PATH}/.ssh/id_rsa.pub"
+    # SSH_PRIVATE_KEY_PATH: str = f"{DIST_PATH}/.ssh/id_rsa"
+    SSH_PUBLIC_KEY_PATH: str = ".ssh/id_rsa.pub"
+    SSH_PRIVATE_KEY_PATH: str = ".ssh/id_rsa"
+    STACK_ID: str = None
+
+    # @validator('SSH_PUBLIC_KEY_PATH')
+    # def ssh_public_key_path_must_end_in_pub(cls, v):
+    #     if v is not None and not v.endswith('.pub'):
+    #         raise ValueError('SSH_PUBLIC_KEY_PATH must end in .pub')
+
+    #     return v
