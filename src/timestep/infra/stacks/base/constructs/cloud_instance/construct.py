6f647347a62ceb4205f8d3aaa2a99d99f7286e89
Michael Schock <m@mjschock.com>
2023-06-24 20:22:54 -0700

    f
    Former-commit-id: 91127dd9e6b196c110c2f9005656ae7f46402e31

--- src/timestep/infra/stacks/base/constructs/cloud_instance/construct.py
+++ src/timestep/infra/stacks/base/constructs/cloud_instance/construct.py
@@ -127,7 +127,8 @@ def get_cloud_instance_resource(scope: TerraformStack, config: MainConfig, cloud
             # user_data=cloud_init_config_construct.outputs["user_data"].value, TypeError: type of argument user_data must be one of (str, NoneType); got cdktf.StringMap instead
             # user_data=cloud_init_config_construct.outputs["user_data"].value.outputs["user_data"].value, AttributeError: 'StringMap' object has no attribute 'outputs'
             # user_data=cloud_init_config_construct.outputs["user_data"].value["user_data"].value TypeError: 'StringMap' object is not subscriptable
-            user_data=cloud_init_config_construct.outputs["user_data"].value["user_data"],
+            # user_data=cloud_init_config_construct.outputs["user_data"].value["user_data"],
+            user_data=cloud_init_config_construct.resource.content
         )
 
     else:
