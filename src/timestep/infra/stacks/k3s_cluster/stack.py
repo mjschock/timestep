c3ba8bb892b388496e39483555a1c7c3d6747891
Michael Schock <m@mjschock.com>
2023-07-04 14:23:59 -0700

    f

--- src/timestep/infra/stacks/k3s_cluster/stack.py
+++ src/timestep/infra/stacks/k3s_cluster/stack.py
@@ -5,9 +5,6 @@ from constructs import Construct
 from prefect import get_run_logger
 
 from timestep.conf.blocks import AppConfig
-from timestep.infra.stacks.k3s_cluster.constructs.cloud_init_config.blocks import (
-    CloudInitConfigConstruct,
-)
 
 
 class K3sClusterStack(TerraformStack):
@@ -15,11 +12,11 @@ class K3sClusterStack(TerraformStack):
         super().__init__(scope, id)
         get_run_logger()
 
-        self.cloud_init_config_construct = CloudInitConfigConstruct(
-            config=config,
-            id="cloud_init_config_construct",
-            scope=self,
-        )
+        # self.cloud_init_config_construct = CloudInitConfigConstruct(
+        #     config=config,
+        #     id="cloud_init_config_construct",
+        #     scope=self,
+        # )
 
         # self.cloud_instance_construct = CloudInstanceConstruct(
         #     cloud_init_config_construct=self.cloud_init_config_construct,
