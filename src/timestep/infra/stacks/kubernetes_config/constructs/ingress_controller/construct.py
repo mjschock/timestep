309310f971d70ad791c06d80a35fedcc118a1384
Michael Schock <m@mjschock.com>
2023-07-06 13:28:35 -0700

    f
    Former-commit-id: ae93d0f817158e7cbdcb8352e5d110715f6144ea

--- src/timestep/infra/stacks/kubernetes_config/constructs/ingress_controller/construct.py
+++ src/timestep/infra/stacks/kubernetes_config/constructs/ingress_controller/construct.py
@@ -2,6 +2,8 @@ from constructs import Construct
 from prefect import get_run_logger
 
 from timestep.conf.blocks import AppConfig
+from timestep.infra.imports.helm.provider import HelmProvider, HelmProviderKubernetes
+from timestep.infra.imports.helm.release import Release, ReleaseSet
 from timestep.infra.imports.kubernetes.provider import (
     KubernetesProvider as KubernetesTerraformProvider,
 )
@@ -13,7 +15,7 @@ class IngressControllerConstruct(Construct):
         super().__init__(id=id, scope=scope)
         get_run_logger()
 
-        KubernetesTerraformProvider(
+        kubernetes_provider = KubernetesTerraformProvider(
             config_context=scope.kube_config_construct.outputs.get(
                 "config_context"
             ).value,
@@ -22,44 +24,47 @@ class IngressControllerConstruct(Construct):
             scope=scope,
         )
 
-        # HelmTerraformProvider(
-        #     id="helm_provider",
-        #     kubernetes=HelmProviderKubernetes(
-        #         # config_context=config.KUBE_CONTEXT,
-        #         # config_path=config.KUBE_CONFIG_PATH,
-        #         config_context=kubernetes_provider.config_context,
-        #         config_path=kubernetes_provider.config_path,
-        #     ),
-        #     scope=scope,
-        # )
+        helm_provider = HelmProvider(
+            id="helm_provider",
+            # helm_driver postgres?
+            kubernetes=HelmProviderKubernetes(
+                # config_context=config.KUBE_CONTEXT,
+                # config_path=config.KUBE_CONFIG_PATH,
+                config_context=kubernetes_provider.config_context,
+                config_path=kubernetes_provider.config_path,
+            ),
+            # registry: HelmProviderRegistry
+            # registry_config_path
+            scope=scope,
+        )
 
-        # HelmReleaseTerraformResource(
-        #     id_="caddy_ingress_controller_helm_release_resource",
-        #     atomic=True,
-        #     chart="caddy-ingress-controller",
-        #     create_namespace=True,
-        #     name="caddy-ingress-controller",
-        #     namespace="caddy-system",
-        #     repository="https://caddyserver.github.io/ingress",
-        #     provider=helm_provider,
-        #     set=[
-        #         # https://github.com/caddyserver/ingress/tree/master/charts/caddy-ingress-controller#values
-        #         ReleaseSet(
-        #             name="ingressController.config.acmeCA",
-        #             value="https://acme-staging-v02.api.letsencrypt.org/directory",
-        #         ),
-        #         ReleaseSet(
-        #             name="ingressController.config.email",
-        #             value="m@mjschock.com",
-        #         ),
-        #         ReleaseSet(
-        #             name="ingressController.config.onDemandTLS",
-        #             value="true",
-        #         ),
-        #         ReleaseSet(
-        #             name="ingressController.verbose",
-        #             value="false",
-        #         ),
-        #     ],
-        #     scope=scope,
-        # )
+        Release(
+            id_="caddy_ingress_controller_helm_release_resource",
+            atomic=True,
+            chart="caddy-ingress-controller",
+            create_namespace=True,
+            name="caddy-ingress-controller",
+            namespace="caddy-system",
+            repository="https://caddyserver.github.io/ingress",
+            provider=helm_provider,
+            set=[
+                # https://github.com/caddyserver/ingress/tree/master/charts/caddy-ingress-controller#values
+                ReleaseSet(
+                    name="ingressController.config.acmeCA",
+                    value="https://acme-staging-v02.api.letsencrypt.org/directory",
+                ),
+                ReleaseSet(
+                    name="ingressController.config.email",
+                    value="m@mjschock.com",
+                ),
+                ReleaseSet(
+                    name="ingressController.config.onDemandTLS",
+                    value="true",
+                ),
+                ReleaseSet(
+                    name="ingressController.verbose",
+                    value="false",
+                ),
+            ],
+            scope=scope,
+        )
