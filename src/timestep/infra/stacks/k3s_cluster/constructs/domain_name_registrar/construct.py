ba41a52cd25b024dfc60b14d615b5b1efc2fc090
Michael Schock <m@mjschock.com>
2023-07-06 19:22:32 -0700

    f
    Former-commit-id: fc622434a3aaca67ec4308054b5ebca47b944d73

--- src/timestep/infra/stacks/k3s_cluster/constructs/domain_name_registrar/construct.py
+++ src/timestep/infra/stacks/k3s_cluster/constructs/domain_name_registrar/construct.py
@@ -2,7 +2,6 @@ from constructs import Construct
 from prefect import get_run_logger
 
 from timestep.conf.blocks import AppConfig
-from timestep.infra.imports.null.provider import NullProvider as NullTerraformProvider
 from timestep.infra.stacks.k3s_cluster.constructs.cloud_instance.blocks import (
     CloudInstanceConstruct,
 )
@@ -18,28 +17,3 @@ class DomainNameRegistrarConstruct(Construct):
     ) -> None:
         super().__init__(scope, id)
         get_run_logger()
-
-        # self.domain_name_registrar_provider_future: PrefectFuture[
-        #     TerraformProvider
-        # ] = get_domain_name_registrar_provider.submit(
-        #     scope=scope,
-        #     config=config,
-        #     cloud_instance_construct=cloud_instance_construct,
-        # )
-        # self.domain_name_registrar_resource_future: PrefectFuture[
-        #     TerraformResource
-        # ] = get_domain_name_registrar_resource.submit(
-        #     scope=scope,
-        #     config=config,
-        #     cloud_instance_construct=cloud_instance_construct,
-        #     domain_name_registrar_provider=self.domain_name_registrar_provider_future,
-        # )
-
-        # self.provider = self.domain_name_registrar_provider_future.result()
-        # self.resource = self.domain_name_registrar_resource_future.result()
-
-        NullTerraformProvider(
-            alias="domain_name_registrar_provider",
-            id="domain_name_registrar_provider",
-            scope=scope,
-        )
