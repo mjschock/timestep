bf9b7d947d399b0671d15ccd55d1f33a491b2b58
Michael Schock <m@mjschock.com>
2023-07-06 19:11:26 -0700

    f
    Former-commit-id: 210e1ef1083c448398e5f664977c27ebc9d32d02

--- src/timestep/infra/stacks/k3s_cluster/constructs/domain_name_registrar/blocks.py
+++ src/timestep/infra/stacks/k3s_cluster/constructs/domain_name_registrar/blocks.py
@@ -1,18 +1,11 @@
-from cdktf import (
-    TerraformProvider,
-    TerraformResource,
-)
 from constructs import Construct
 from prefect import get_run_logger
-from prefect.futures import PrefectFuture
 
 from timestep.conf.blocks import AppConfig
+from timestep.infra.imports.null.provider import NullProvider as NullTerraformProvider
+from timestep.infra.imports.null.resource import Resource as NullTerraformResource
 from timestep.infra.stacks.k3s_cluster.constructs.cloud_instance.blocks import (
     CloudInstanceConstruct,
-)
-from timestep.infra.stacks.k3s_cluster.constructs.domain_name_registrar.tasks import (
-    get_domain_name_registrar_provider,
-    get_domain_name_registrar_resource,
 )
 
 
@@ -27,21 +20,33 @@ class DomainNameRegistrarConstruct(Construct):
         super().__init__(scope, id)
         get_run_logger()
 
-        self.domain_name_registrar_provider_future: PrefectFuture[
-            TerraformProvider
-        ] = get_domain_name_registrar_provider.submit(
+        # self.domain_name_registrar_provider_future: PrefectFuture[
+        #     TerraformProvider
+        # ] = get_domain_name_registrar_provider.submit(
+        #     scope=scope,
+        #     config=config,
+        #     cloud_instance_construct=cloud_instance_construct,
+        # )
+        # self.domain_name_registrar_resource_future: PrefectFuture[
+        #     TerraformResource
+        # ] = get_domain_name_registrar_resource.submit(
+        #     scope=scope,
+        #     config=config,
+        #     cloud_instance_construct=cloud_instance_construct,
+        #     domain_name_registrar_provider=self.domain_name_registrar_provider_future,
+        # )
+
+        # self.provider = self.domain_name_registrar_provider_future.result()
+        # self.resource = self.domain_name_registrar_resource_future.result()
+
+        domain_name_registrar_provider = NullTerraformProvider(
+            alias="domain_name_registrar_provider",
+            id="domain_name_registrar_provider",
             scope=scope,
-            config=config,
-            cloud_instance_construct=cloud_instance_construct,
-        )
-        self.domain_name_registrar_resource_future: PrefectFuture[
-            TerraformResource
-        ] = get_domain_name_registrar_resource.submit(
-            scope=scope,
-            config=config,
-            cloud_instance_construct=cloud_instance_construct,
-            domain_name_registrar_provider=self.domain_name_registrar_provider_future,
         )
 
-        self.provider = self.domain_name_registrar_provider_future.result()
-        self.resource = self.domain_name_registrar_resource_future.result()
+        NullTerraformResource(
+            id="domain_name_registrar_resource",
+            provider=domain_name_registrar_provider,
+            scope=scope,
+        )
