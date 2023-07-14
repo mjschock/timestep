91918dfd45a60ad5a1efe1988dd19d02f6252f37
Michael Schock <m@mjschock.com>
2023-07-13 19:50:19 -0700

    f
    Former-commit-id: ccbb090e14fd6bbb306a6cd7be57607120f54dd5

--- src/timestep/infra/stacks/k3s_cluster/stack.py
+++ src/timestep/infra/stacks/k3s_cluster/stack.py
@@ -14,7 +14,7 @@ from timestep.infra.stacks.k3s_cluster.constructs.cloud_init_config.blocks impor
 from timestep.infra.stacks.k3s_cluster.constructs.cloud_instance.blocks import (
     CloudInstanceConstruct,
 )
-from timestep.infra.stacks.k3s_cluster.constructs.cloud_instance_domain.blocks import (
+from timestep.infra.stacks.k3s_cluster.constructs.cloud_instance_domain.construct import (  # noqa: E501
     CloudInstanceDomainConstruct,
 )
 from timestep.infra.stacks.k3s_cluster.constructs.domain_name_registrar.construct import (  # noqa: E501
@@ -53,48 +53,6 @@ class K3sClusterStack(TerraformStack):
             id="domain_name_registar_construct",
             scope=self,
         )
-
-        # self.kube_config_construct = KubeConfigConstruct(
-        #     cloud_instance_construct=self.cloud_instance_construct,
-        #     config=config,
-        #     id="kube_config_construct",
-        #     scope=self,
-        # )
-
-        # self.container_registry_construct = ContainerRegistryConstruct(
-        #     config=config,
-        #     id="container_registry_construct",
-        #     scope=self,
-        # )
-
-        # self.ingress_controller_construct = IngressControllerConstruct(
-        #     # container_registry_construct=self.container_registry_construct,
-        #     config=config,
-        #     kube_config_construct=self.kube_config_construct,
-        #     id="ingress_controller_construct",
-        #     scope=self,
-        # )
-
-        # self.minio_construct = MinioConstruct(
-        #     ingress_controller_construct=self.ingress_controller_construct,
-        #     config=config,
-        #     id="minio_construct",
-        #     scope=self,
-        # )
-
-        # self.postgres_construct = PostgresConstruct(
-        #     ingress_controller_construct=self.ingress_controller_construct,
-        #     config=config,
-        #     id="postgres_construct",
-        #     scope=self,
-        # )
-
-        # self.telemetry_construct = TelemetryConstruct(
-        #     ingress_controller_construct=self.ingress_controller_construct,
-        #     config=config,
-        #     id="telemetry_construct",
-        #     scope=self,
-        # )
 
         stack_id: str = config.variables.get("primary_domain_name")
 
