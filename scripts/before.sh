0dcfbcbdcc7a3b3a64eec1b4939f570a77f7417d
Michael Schock <m@mjschock.com>
2023-08-12 14:27:22 -0700

    f
    Former-commit-id: d13fc4ce0e7d6a75e92c0d103df2aa3177cf84a3

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 set -x # echo on
 
+echo $CI_REGISTRY_USER
+echo '$CI_REGISTRY_USER'
+echo ${CI_REGISTRY_USER}
+echo '${CI_REGISTRY_USER}'
+
 docker login -u '$CI_REGISTRY_USER' -p '$CI_REGISTRY_PASSWORD' '$CI_REGISTRY'
 docker pull '$CI_REGISTRY_IMAGE':latest || true
