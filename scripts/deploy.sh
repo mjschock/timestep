f76e559eb4d259f65d2012a63a0f2746968dcf03
Michael Schock <m@mjschock.com>
2023-08-12 11:44:43 -0700

    f
    Former-commit-id: a0faa74cb6e12cde4935e154d047dc1bd85c4444

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker run \
+ --env-file .env \
+ --env CLOUD_INSTANCE_PROVIDER=$CLOUD_INSTANCE_PROVIDER \
+ --env HTPASSWD=$HTPASSWD \
+ --env PRIMARY_DOMAIN_NAME=$PRIMARY_DOMAIN_NAME \
+ --env SSH_PRIVATE_KEY=$SSH_PRIVATE_KEY \
+ --env TF_API_TOKEN=$CI_JOB_TOKEN \
+ $CI_REGISTRY_IMAGE:latest poetry run cdktf deploy --auto-approve $PRIMARY_DOMAIN_NAME
