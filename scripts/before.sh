f290612375fadf3e97aefd589981588163e5dfbd
Michael Schock <m@mjschock.com>
2023-08-12 14:07:41 -0700

    f

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -x # echo on
 
-docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
-docker pull $CI_REGISTRY_IMAGE:latest || true
+docker login -u '$CI_REGISTRY_USER' -p '$CI_REGISTRY_PASSWORD' '$CI_REGISTRY'
+docker pull '$CI_REGISTRY_IMAGE':latest || true
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,9 @@ set -x # echo on
 
 docker run \
  --env-file .env \
- --env CLOUD_INSTANCE_PROVIDER=$CLOUD_INSTANCE_PROVIDER \
- --env HTPASSWD=$HTPASSWD \
- --env PRIMARY_DOMAIN_NAME=$PRIMARY_DOMAIN_NAME \
- --env SSH_PRIVATE_KEY=$SSH_PRIVATE_KEY \
- --env TF_API_TOKEN=$CI_JOB_TOKEN \
- $CI_REGISTRY_IMAGE:latest poetry run cdktf deploy --auto-approve $PRIMARY_DOMAIN_NAME
+ --env CLOUD_INSTANCE_PROVIDER='$CLOUD_INSTANCE_PROVIDER' \
+ --env HTPASSWD='$HTPASSWD' \
+ --env PRIMARY_DOMAIN_NAME='$PRIMARY_DOMAIN_NAME' \
+ --env SSH_PRIVATE_KEY='$SSH_PRIVATE_KEY' \
+ --env TF_API_TOKEN='$CI_JOB_TOKEN' \
+ '$CI_REGISTRY_IMAGE':latest poetry run cdktf deploy --auto-approve '$PRIMARY_DOMAIN_NAME'
