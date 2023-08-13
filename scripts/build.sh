886c50e430f556e1dbc470389cf79b1ea6995a28
Michael Schock <m@mjschock.com>
2023-08-13 12:55:38 -0700

    fix: f

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,8 +7,8 @@ set -x # echo on
 # docker push ${CI_REGISTRY_IMAGE}:latest
 # docker compose pull || true
 docker buildx build --push -t ${CI_REGISTRY_IMAGE}:latest \
-  --cache-to type=registry,ref=${CI_REGISTRY_IMAGE}:latest \
-  --cache-from type=registry,ref=${CI_REGISTRY_IMAGE}:latest .
+  --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}:latest \
+  --cache-from type=inline,ref=${CI_REGISTRY_IMAGE}:latest .
 
 # docker build --cache-from registry.gitlab.com/timestep-ai/caddy:latest --tag ${CI_REGISTRY_IMAGE}:${CI_COMMIT_SHA} --tag ${CI_REGISTRY_IMAGE}:latest .
 # docker compose build
