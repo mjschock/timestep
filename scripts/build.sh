39be8d862db293ed4d1cca23ad0ae5565463c455
Michael Schock <m@mjschock.com>
2023-08-13 12:03:41 -0700

    f

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,7 @@ docker push ${CI_REGISTRY_IMAGE}:latest
 # docker compose pull || true
 
 # docker build --cache-from registry.gitlab.com/timestep-ai/caddy:latest --tag ${CI_REGISTRY_IMAGE}:${CI_COMMIT_SHA} --tag ${CI_REGISTRY_IMAGE}:latest .
-docker compose build
-docker compose push
+# docker compose build
+# docker compose push
+docker buildx bake --pull --push
 # docker run timestep kompose convert --build local --build-command "helm package timestep-ai" --chart --file docker-compose.yml --push-image --push-command "helm push timestep-ai-0.0.1.tgz oci://registry.gitlab.com/timestep-ai/timestep" --push-image-registry registry.gitlab.com --out timestep-ai --secrets-as-files --verbose'
