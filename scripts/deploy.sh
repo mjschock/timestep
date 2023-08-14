5ed7536d2d6fca8a250cc7feb64b8c6eb4a7062d
Michael Schock <m@mjschock.com>
2023-08-14 10:50:39 -0700

    fix: f

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,4 +23,20 @@ docker run \
  --env TF_USERNAME=${TF_USERNAME} \
  --user $(id -u):$(id -g) \
  --volume $(pwd)/secrets:/home/ubuntu/secrets:ro \
+ ${CI_REGISTRY_IMAGE}:latest kompose convert --chart --file docker-compose.yml --generate-network-policies --out timestep-ai --secrets-as-files --verbose
+
+docker run \
+ --env-file .env \
+ --env CLOUD_INSTANCE_PROVIDER=${CLOUD_INSTANCE_PROVIDER} \
+ --env DO_TOKEN=${DO_TOKEN} \
+ --env DOCKER_REGISTRY_PASSWORD=${DOCKER_REGISTRY_PASSWORD} \
+ --env HTPASSWD=${HTPASSWD} \
+ --env KUBECONFIG=${KUBECONFIG} \
+ --env KUBECONTEXT=${KUBECONTEXT} \
+ --env POSTGRESQL_PASSWORD=${POSTGRESQL_PASSWORD} \
+ --env PRIMARY_DOMAIN_NAME=${PRIMARY_DOMAIN_NAME} \
+ --env TF_API_TOKEN=${TF_API_TOKEN} \
+ --env TF_USERNAME=${TF_USERNAME} \
+ --user $(id -u):$(id -g) \
+ --volume $(pwd)/secrets:/home/ubuntu/secrets:ro \
  ${CI_REGISTRY_IMAGE}:latest poetry run cdktf deploy --auto-approve ${PRIMARY_DOMAIN_NAME}
