ef08fc1f41d3ad10600233d377594008772b48d2
Michael Schock <m@mjschock.com>
2023-08-13 18:49:42 -0700

    f
    Former-commit-id: b0ae507857a98ff734edbf8d0c335c074cfc5def

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 set -x # echo on
 
-mkdir -p secrets
-# echo ${SSH_PRIVATE_KEY} > secrets/ssh_private_key
-echo ${SSH_PRIVATE_KEY_ENV_VAR} > secrets/ssh_private_key
-cat secrets/ssh_private_key
-ls -al secrets
-# chmod 600 secrets/ssh_private_key
-id
+# mkdir -p secrets
+# # echo ${SSH_PRIVATE_KEY} > secrets/ssh_private_key
+# echo ${SSH_PRIVATE_KEY_ENV_VAR} > secrets/ssh_private_key
+# cat secrets/ssh_private_key
+# ls -al secrets
+# # chmod 600 secrets/ssh_private_key
+# id
 
 docker run \
  --env-file .env \
