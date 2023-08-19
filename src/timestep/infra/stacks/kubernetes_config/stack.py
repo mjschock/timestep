780ab4d2b5d150537c233b6098c68a276d2826d9
Michael Schock <m@mjschock.com>
2023-08-18 21:04:40 -0700

    fix: f

--- src/timestep/infra/stacks/kubernetes_config/stack.py
+++ src/timestep/infra/stacks/kubernetes_config/stack.py
@@ -11,9 +11,6 @@ from timestep.config import CloudInstanceProvider
 from timestep.infra.stacks.kubernetes_config.constructs.kubernetes_cluster_ingress.construct import (  # noqa: E501
     KubernetesClusterIngressConstruct,
 )
-from timestep.infra.stacks.kubernetes_config.constructs.timestep_ai.construct import (
-    TimestepAIConstruct,
-)
 
 
 class KubernetesConfigStack(TerraformStack):
@@ -59,12 +56,12 @@ class KubernetesConfigStack(TerraformStack):
         #     )
         # )
 
-        self.timestep_ai_contruct: TimestepAIConstruct = TimestepAIConstruct(
-            config=config,
-            id="timestep_ai_contruct",
-            helm_provider=self.helm_provider,
-            scope=self,
-        )
+        # self.timestep_ai_contruct: TimestepAIConstruct = TimestepAIConstruct(
+        #     config=config,
+        #     id="timestep_ai_contruct",
+        #     helm_provider=self.helm_provider,
+        #     scope=self,
+        # )
 
         if config.cloud_instance_provider == CloudInstanceProvider.MULTIPASS:
             LocalBackend(
