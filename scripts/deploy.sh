b30fb70c4e7eb86e33e543bb91af347c9e34ce2d
Michael Schock <m@mjschock.com>
2023-08-18 18:28:49 -0700

    fix: Adds volume for Helm chart
    Former-commit-id: 325d7dcf2e18e98f644ac6c808b7e959d3dab2a9

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,35 +21,8 @@ docker run \
  --env TF_USERNAME=${TF_USERNAME} \
  --user $(id -u):$(id -g) \
  --volume $(pwd)/secrets:/home/ubuntu/secrets:rw \
+ --volume $(pwd)/timestep-ai:/home/ubuntu/timestep-ai:rw \
  ${CI_REGISTRY_IMAGE}:latest kompose convert --chart --file docker-compose.yml --out timestep-ai --secrets-as-files --verbose
-
-# docker run \
-#  --env-file .env \
-#  --env CLOUD_INSTANCE_PROVIDER=${CLOUD_INSTANCE_PROVIDER} \
-#  --env DO_TOKEN=${DO_TOKEN} \
-#  --env DOCKER_REGISTRY_PASSWORD=${DOCKER_REGISTRY_PASSWORD} \
-#  --env HTPASSWD=${HTPASSWD} \
-#  --env KUBECONTEXT=${KUBECONTEXT} \
-#  --env PRIMARY_DOMAIN_NAME=${PRIMARY_DOMAIN_NAME} \
-#  --env TF_API_TOKEN=${TF_API_TOKEN} \
-#  --env TF_USERNAME=${TF_USERNAME} \
-#  --user $(id -u):$(id -g) \
-#  --volume $(pwd)/secrets:/home/ubuntu/secrets:rw \
-#  ${CI_REGISTRY_IMAGE}:latest poetry run cdktf deploy --auto-approve ${PRIMARY_DOMAIN_NAME}.k3s_cluster
-
-# docker run \
-#  --env-file .env \
-#  --env CLOUD_INSTANCE_PROVIDER=${CLOUD_INSTANCE_PROVIDER} \
-#  --env DO_TOKEN=${DO_TOKEN} \
-#  --env DOCKER_REGISTRY_PASSWORD=${DOCKER_REGISTRY_PASSWORD} \
-#  --env HTPASSWD=${HTPASSWD} \
-#  --env KUBECONTEXT=${KUBECONTEXT} \
-#  --env PRIMARY_DOMAIN_NAME=${PRIMARY_DOMAIN_NAME} \
-#  --env TF_API_TOKEN=${TF_API_TOKEN} \
-#  --env TF_USERNAME=${TF_USERNAME} \
-#  --user $(id -u):$(id -g) \
-#  --volume $(pwd)/secrets:/home/ubuntu/secrets:rw \
-#  ${CI_REGISTRY_IMAGE}:latest poetry run cdktf deploy --auto-approve --ignore-missing-stack-dependencies ${PRIMARY_DOMAIN_NAME}.kubernetes_config
 
 docker run \
  --env-file .env \
@@ -63,5 +36,6 @@ docker run \
  --env TF_USERNAME=${TF_USERNAME} \
  --user $(id -u):$(id -g) \
  --volume $(pwd)/secrets:/home/ubuntu/secrets:rw \
+ --volume $(pwd)/timestep-ai:/home/ubuntu/timestep-ai:rw \
  ${CI_REGISTRY_IMAGE}:latest poetry run cdktf deploy --auto-approve ${PRIMARY_DOMAIN_NAME}.k3s_cluster ${PRIMARY_DOMAIN_NAME}.kubernetes_config
  
