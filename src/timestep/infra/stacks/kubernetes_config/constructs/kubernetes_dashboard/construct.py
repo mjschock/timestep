604c17a86b02350c403efdf4458db7e6d2fbaa7c
Michael Schock <m@mjschock.com>
2023-08-20 13:33:17 -0700

    feat: Adds Kubernetes Dashboard

--- src/timestep/infra/stacks/kubernetes_config/constructs/kubernetes_dashboard/construct.py
+++ src/timestep/infra/stacks/kubernetes_config/constructs/kubernetes_dashboard/construct.py
@@ -1,5 +1,16 @@
 from cdktf_cdktf_provider_helm.provider import HelmProvider
 from cdktf_cdktf_provider_helm.release import Release
+from cdktf_cdktf_provider_kubernetes.ingress_v1 import (
+    IngressV1,
+    IngressV1Metadata,
+    IngressV1Spec,
+    IngressV1SpecRule,
+    IngressV1SpecRuleHttp,
+    IngressV1SpecRuleHttpPath,
+    IngressV1SpecRuleHttpPathBackend,
+    IngressV1SpecRuleHttpPathBackendService,
+    IngressV1SpecRuleHttpPathBackendServicePort,
+)
 from constructs import Construct
 from timestep.config import Settings
 
@@ -38,6 +49,118 @@ class KubernetesDashboardConstruct(Construct):
                 },
             ],
             scope=self,
+        )
+
+        IngressV1(
+            depends_on=[
+                self.kubernetes_dashboard_helm_release_resource,
+            ],
+            id_="kubernetes_dashboard_ingress",
+            metadata=IngressV1Metadata(
+                annotations={
+                    # "cert-manager.io/cluster-issuer": cert_manager_cluster_issuer,
+                    # "cert-manager.io/issuer": issuer_type,
+                    # "ingress.kubernetes.io/proxy-body-size": "0",
+                    # "ingress.kubernetes.io/ssl-redirect": "true",
+                    "kubernetes.io/ingress.class": "caddy",
+                    # "meta.helm.sh/release-name": helm_release_name,
+                    # "meta.helm.sh/release-namespace": helm_release_namespace,
+                    # f"{ingress_class}.ingress.kubernetes.io/proxy-body-size": "0",
+                    # f"{ingress_class}.ingress.kubernetes.io/ssl-redirect": "true",
+                },
+                # labels={
+                #     "app.kubernetes.io/instance": helm_release_name,
+                #     "app.kubernetes.io/managed-by": "Helm",
+                #     "app.kubernetes.io/name": helm_release_name,
+                #     "helm.sh/chart": f"{helm_release_name}-{helm_release_chart_version}",  # noqa: E501
+                # },
+                name="kubernetes-dashboard",
+                namespace="kubernetes-dashboard",
+                # namespace=namespace,
+            ),
+            scope=self,
+            spec=IngressV1Spec(
+                # ingress_class_name=ingress_class,
+                rule=[
+                    IngressV1SpecRule(
+                        host=f"kubernetes-dashboard.{config.primary_domain_name}",
+                        http=IngressV1SpecRuleHttp(
+                            path=[
+                                IngressV1SpecRuleHttpPath(
+                                    backend=IngressV1SpecRuleHttpPathBackend(
+                                        service=IngressV1SpecRuleHttpPathBackendService(
+                                            name="kubernetes-dashboard",
+                                            port=IngressV1SpecRuleHttpPathBackendServicePort(
+                                                # name=path["service_port_name"],
+                                                number=443,
+                                            ),
+                                        ),
+                                    ),
+                                    path="/",
+                                    path_type="Prefix",
+                                    # path_type=path["path_type"],
+                                ),
+                                # IngressV1SpecRuleHttpPath(
+                                #     backend=IngressV1SpecRuleHttpPathBackend(
+                                #         service=IngressV1SpecRuleHttpPathBackendService(  # noqa: E501
+                                #             name="example2",
+                                #             port=IngressV1SpecRuleHttpPathBackendServicePort(  # noqa: E501
+                                #                 # name=path["service_port_name"],
+                                #                 number=8080,
+                                #             ),
+                                #         ),
+                                #     ),
+                                #     path="/hello2",
+                                #     path_type="Prefix",
+                                #     # path_type=path["path_type"],
+                                # ),
+                            ]
+                        ),
+                    ),
+                    # IngressV1SpecRule(
+                    #     host=f"example2.{config.primary_domain_name}",
+                    #     http=IngressV1SpecRuleHttp(
+                    #         path=[
+                    #             IngressV1SpecRuleHttpPath(
+                    #                 backend=IngressV1SpecRuleHttpPathBackend(
+                    #                     service=IngressV1SpecRuleHttpPathBackendService(  # noqa: E501
+                    #                         name="example1",
+                    #                         port=IngressV1SpecRuleHttpPathBackendServicePort(  # noqa: E501
+                    #                             # name=path["service_port_name"],
+                    #                             number=8080,
+                    #                         ),
+                    #                     ),
+                    #                 ),
+                    #                 path="/hello1",
+                    #                 path_type="Prefix",
+                    #                 # path_type=path["path_type"],
+                    #             ),
+                    #             IngressV1SpecRuleHttpPath(
+                    #                 backend=IngressV1SpecRuleHttpPathBackend(
+                    #                     service=IngressV1SpecRuleHttpPathBackendService(  # noqa: E501
+                    #                         name="example2",
+                    #                         port=IngressV1SpecRuleHttpPathBackendServicePort(  # noqa: E501
+                    #                             # name=path["service_port_name"],
+                    #                             number=8080,
+                    #                         ),
+                    #                     ),
+                    #                 ),
+                    #                 path="/hello2",
+                    #                 path_type="Prefix",
+                    #                 # path_type=path["path_type"],
+                    #             ),
+                    #         ]
+                    #     ),
+                    # )
+                ],
+                # tls=[
+                #     IngressV1SpecTls(
+                #         hosts=[host],
+                #         # secret_name=f"{host}-tls",
+                #         secret_name=f"{ingress_name}-tls",
+                #     )
+                # ],
+            ),
         )
 
         # IngressV1(
