d1c16d1023fc1fc7827fa7c67db3b9daeb59c3d0
Michael Schock <m@mjschock.com>
2023-08-20 13:33:17 -0700

    feat: Adds Kubernetes Dashboard
    Former-commit-id: 604c17a86b02350c403efdf4458db7e6d2fbaa7c

--- src/timestep/infra/stacks/k3s_cluster/constructs/cloud_instance_domain/construct.py
+++ src/timestep/infra/stacks/k3s_cluster/constructs/cloud_instance_domain/construct.py
@@ -30,6 +30,7 @@ class CloudInstanceDomainConstruct(Construct):
         super().__init__(scope, id)
 
         subdomains = [
+            "kubernetes-dashboard",
             "prefect-server",
             "www",
         ]
