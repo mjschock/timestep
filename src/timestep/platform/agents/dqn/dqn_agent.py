9e302f7232b90b64e9e0046547ee87b9cd7bda3a
Michael Schock <m@mjschock.com>
2023-09-30 09:32:40 -0700

    bak

--- src/timestep/platform/agents/dqn/dqn_agent.py
+++ src/timestep/platform/agents/dqn/dqn_agent.py
@@ -2,6 +2,7 @@ import random
 from collections import deque, namedtuple
 
 import numpy as np
+
 # import torch
 # import torch.nn as nn
 # import torch.nn.functional as F
@@ -86,7 +87,7 @@ class Agent():
 
         # Get max predicted Q values (for next states) from target model
         Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
-        # Compute Q targets for current states 
+        # Compute Q targets for current states
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
         # Get expected Q values from local model
@@ -130,7 +131,7 @@ class ReplayBuffer:
             seed (int): random seed
         """
         self.action_size = action_size
-        self.memory = deque(maxlen=buffer_size)  
+        self.memory = deque(maxlen=buffer_size)
         self.batch_size = batch_size
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
         self.seed = random.seed(seed)
