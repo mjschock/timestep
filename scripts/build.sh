f56ddd98f923282ded8bfeb4935c9a9231e5f8d1
Michael Schock <m@mjschock.com>
2023-10-14 16:39:38 -0700

    fix: f

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -x # echo on
 
+docker login -u ${DOCKER_REGISTRY_USERNAME} -p ${DOCKER_REGISTRY_PASSWORD} ${DOCKER_REGISTRY_SERVER}
+
 docker buildx build \
   --cache-from ${CI_REGISTRY_IMAGE}:latest \
   --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}:latest \
