6c5ea826be8cb96d40ea46b9bc75e4dadd5637bf
Michael Schock <m@mjschock.com>
2023-10-25 18:34:11 -0700

    fix: f

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,21 +19,21 @@ docker login -u ${DOCKER_REGISTRY_USERNAME} -p ${DOCKER_REGISTRY_PASSWORD} ${DOC
 #  --volume $(pwd)/timestep-ai:/home/ubuntu/timestep-ai:rw \
 #  ${CI_REGISTRY_IMAGE}:latest kompose convert --chart --file docker-compose.yml --out timestep-ai --secrets-as-files --verbose
 
-docker run \
- --env-file .env \
- --env CLOUD_INSTANCE_PROVIDER=${CLOUD_INSTANCE_PROVIDER} \
- --env DO_TOKEN=${DO_TOKEN} \
- --env DOCKER_REGISTRY_PASSWORD=${DOCKER_REGISTRY_PASSWORD} \
- --env HTPASSWD=${HTPASSWD} \
- --env KUBECONTEXT=${KUBECONTEXT} \
- --env PRIMARY_DOMAIN_NAME=${PRIMARY_DOMAIN_NAME} \
- --env TF_API_TOKEN=${TF_API_TOKEN} \
- --env TF_USERNAME=${TF_USERNAME} \
- --user $(id -u):$(id -g) \
- --volume $(pwd)/secrets:/home/ubuntu/secrets:rw \
- --volume $(pwd)/timestep-ai:/home/ubuntu/timestep-ai:rw \
- --volume $(pwd)/dist:/home/ubuntu/dist:rw \
- ${CI_REGISTRY_IMAGE}:latest helm package timestep-ai --destination /home/ubuntu/dist --version $(cat .env | grep ^VERSION | cut -d '=' -f2)
+# docker run \
+#  --env-file .env \
+#  --env CLOUD_INSTANCE_PROVIDER=${CLOUD_INSTANCE_PROVIDER} \
+#  --env DO_TOKEN=${DO_TOKEN} \
+#  --env DOCKER_REGISTRY_PASSWORD=${DOCKER_REGISTRY_PASSWORD} \
+#  --env HTPASSWD=${HTPASSWD} \
+#  --env KUBECONTEXT=${KUBECONTEXT} \
+#  --env PRIMARY_DOMAIN_NAME=${PRIMARY_DOMAIN_NAME} \
+#  --env TF_API_TOKEN=${TF_API_TOKEN} \
+#  --env TF_USERNAME=${TF_USERNAME} \
+#  --user $(id -u):$(id -g) \
+#  --volume $(pwd)/secrets:/home/ubuntu/secrets:rw \
+#  --volume $(pwd)/timestep-ai:/home/ubuntu/timestep-ai:rw \
+#  --volume $(pwd)/dist:/home/ubuntu/dist:rw \
+#  ${CI_REGISTRY_IMAGE}:latest helm package timestep-ai --destination /home/ubuntu/dist --version $(cat .env | grep ^VERSION | cut -d '=' -f2)
 
 docker run \
  --env-file .env \
