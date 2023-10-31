73246a03aa2c63309bf758c17e36b85daa6ab8c4
Michael Schock <m@mjschock.com>
2023-10-30 20:51:02 -0700

    fix: f
    Former-commit-id: 418ebd7f2497d5dfeeb15b4d1605cf62c6d237cf

--- src/timestep/services/frontend/src/apollo/index.ts
+++ src/timestep/services/frontend/src/apollo/index.ts
@@ -10,11 +10,11 @@ export /* async */ function getClientOptions(
     <ApolloClientOptions<unknown>>{
       link: createHttpLink({
         uri:
-          process.env.GRAPHQL_URI ||
+          // process.env.GRAPHQL_URI ||
           // Change to your graphql endpoint.
-          'https://example.com/graphql',
+          // 'https://example.com/graphql',
+          `https://www.${process.env.PRIMARY_DOMAIN_NAME}/graphql`,
       }),
-
       cache: new InMemoryCache(),
     },
 
