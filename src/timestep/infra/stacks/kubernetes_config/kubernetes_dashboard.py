288c35913de9c3ab19d2aedf3a0db928327a9b5d
Michael Schock <m@mjschock.com>
2023-10-31 10:56:14 -0700

    fix: f

--- src/timestep/infra/stacks/kubernetes_config/kubernetes_dashboard.py
+++ src/timestep/infra/stacks/kubernetes_config/kubernetes_dashboard.py
@@ -1,16 +1,5 @@
 from cdktf_cdktf_provider_helm.provider import HelmProvider
 from cdktf_cdktf_provider_helm.release import Release, ReleaseSet
-from cdktf_cdktf_provider_kubernetes.ingress_v1 import (
-    IngressV1,
-    IngressV1Metadata,
-    IngressV1Spec,
-    IngressV1SpecRule,
-    IngressV1SpecRuleHttp,
-    IngressV1SpecRuleHttpPath,
-    IngressV1SpecRuleHttpPathBackend,
-    IngressV1SpecRuleHttpPathBackendService,
-    IngressV1SpecRuleHttpPathBackendServicePort,
-)
 from constructs import Construct
 
 from timestep.config import Settings
@@ -52,40 +41,40 @@ class KubernetesDashboardConstruct(Construct):
             scope=self,
         )
 
-        IngressV1(
-            depends_on=[
-                self.kubernetes_dashboard_helm_release_resource,
-            ],
-            id_="kubernetes_dashboard_ingress",
-            metadata=IngressV1Metadata(
-                annotations={
-                    "kubernetes.io/ingress.class": "caddy",
-                },
-                name="kubernetes-dashboard",
-                namespace="kubernetes-dashboard",
-            ),
-            scope=self,
-            spec=IngressV1Spec(
-                rule=[
-                    IngressV1SpecRule(
-                        host=f"kubernetes-dashboard.{config.primary_domain_name}",
-                        http=IngressV1SpecRuleHttp(
-                            path=[
-                                IngressV1SpecRuleHttpPath(
-                                    backend=IngressV1SpecRuleHttpPathBackend(
-                                        service=IngressV1SpecRuleHttpPathBackendService(
-                                            name="kubernetes-dashboard",
-                                            port=IngressV1SpecRuleHttpPathBackendServicePort(
-                                                number=443,  # https://artifacthub.io/packages/helm/k8s-dashboard/kubernetes-dashboard#networkpolicy
-                                            ),
-                                        ),
-                                    ),
-                                    path="/",
-                                    path_type="Prefix",
-                                ),
-                            ]
-                        ),
-                    ),
-                ],
-            ),
-        )
+        # IngressV1(
+        #     depends_on=[
+        #         self.kubernetes_dashboard_helm_release_resource,
+        #     ],
+        #     id_="kubernetes_dashboard_ingress",
+        #     metadata=IngressV1Metadata(
+        #         annotations={
+        #             "kubernetes.io/ingress.class": "caddy",
+        #         },
+        #         name="kubernetes-dashboard",
+        #         namespace="kubernetes-dashboard",
+        #     ),
+        #     scope=self,
+        #     spec=IngressV1Spec(
+        #         rule=[
+        #             IngressV1SpecRule(
+        #                 host=f"kubernetes-dashboard.{config.primary_domain_name}",
+        #                 http=IngressV1SpecRuleHttp(
+        #                     path=[
+        #                         IngressV1SpecRuleHttpPath(
+        #                             backend=IngressV1SpecRuleHttpPathBackend(
+        #                                 service=IngressV1SpecRuleHttpPathBackendService(  # noqa: E501
+        #                                     name="kubernetes-dashboard",
+        #                                     port=IngressV1SpecRuleHttpPathBackendServicePort(  # noqa: E501
+        #                                         number=443,  # https://artifacthub.io/packages/helm/k8s-dashboard/kubernetes-dashboard#networkpolicy
+        #                                     ),
+        #                                 ),
+        #                             ),
+        #                             path="/",
+        #                             path_type="Prefix",
+        #                         ),
+        #                     ]
+        #                 ),
+        #             ),
+        #         ],
+        #     ),
+        # )
--- src/timestep/infra/stacks/kubernetes_config/postgresql.py
+++ src/timestep/infra/stacks/kubernetes_config/postgresql.py
@@ -1,5 +1,5 @@
 from cdktf_cdktf_provider_helm.provider import HelmProvider
-from cdktf_cdktf_provider_helm.release import Release, ReleaseSet, ReleaseSetSensitive
+from cdktf_cdktf_provider_helm.release import Release, ReleaseSetSensitive
 from constructs import Construct
 
 from timestep.config import Settings
@@ -16,67 +16,18 @@ class PostgreSQLConstruct(Construct):
         super().__init__(scope, id)
 
         self.release_resource = Release(
-            depends_on=[self.kubernetes_cluster_ingress_construct],
             id_="postgresql_helm_release_resource",
             atomic=True,
-            # chart="postgresql",
-            chart="supabase",
+            chart="postgresql-ha",
             create_namespace=True,
             name="postgresql",
-            namespace="postgresql",
+            namespace="default",
             repository="https://charts.bitnami.com/bitnami",
-            provider=self.kubernetes_cluster_ingress_construct.helm_provider,
-            set=[
-                # ReleaseSet(
-                #     name="architecture",
-                #     value="standalone",
-                # ),
-                ReleaseSet(
-                    name="auth.enabled",
-                    value="false",
-                ),
-                ReleaseSet(
-                    name="meta.enabled",
-                    value="false",
-                ),
-                ReleaseSet(
-                    name="realtime.enabled",
-                    value="false",
-                ),
-                ReleaseSet(
-                    name="rest.enabled",
-                    value="false",
-                ),
-                ReleaseSet(
-                    name="storage.enabled",
-                    value="false",
-                ),
-                ReleaseSet(
-                    name="studio.enabled",
-                    value="false",
-                ),
-                ReleaseSet(
-                    name="kong.enabled",
-                    value="false",
-                ),
-                ReleaseSet(
-                    name="postgresql.enabled",
-                    value="true",
-                ),
-                ReleaseSet(
-                    name="postgresql.architecture",
-                    value="standalone",
-                ),
-            ],
+            provider=helm_provider,
+            set=[],
             set_sensitive=[
-                # ReleaseSetSensitive(
-                #     name="auth.postgresPassword",
-                #     value=config.secrets.get_secret_value().get(
-                #         "postgresql_password"
-                #     ),  # noqa: E501
-                # )
                 ReleaseSetSensitive(
-                    name="postgresql.auth.postgresPassword",
+                    name="postgresql.password",
                     value=config.postgresql_password.get_secret_value(),
                 )
             ],
