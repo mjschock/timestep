de169713317d54b543b0fe46e1701892a60f66d1
Michael Schock <m@mjschock.com>
2023-12-04 18:37:05 -0800

    testing
    Former-commit-id: 71ca899623e24a6b52c311ecc2a25297e73dac5b

--- src/timestep/infra/cicd/build.sh
+++ src/timestep/infra/cicd/build.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e # exit on first error
 set -x # echo on
+
+json_secrets=$1
+echo "json_secrets: ${json_secrets}"
 
 # TODO: Load these using direnv w/ dotenv
 CDKTF_CLI_VERSION=$(cat .env | grep ^CDKTF_CLI_VERSION | cut -d '=' -f2)
@@ -8,57 +11,57 @@ VERSION=$(cat .env | grep ^VERSION | cut -d '=' -f2)
 
 docker login -u ${DOCKER_REGISTRY_USERNAME} -p ${DOCKER_REGISTRY_PASSWORD} ${DOCKER_REGISTRY_SERVER}
 
-if [ -z ${IMAGE_NAME+x} ]; then
-  docker buildx build \
-    --build-arg CDKTF_CLI_VERSION=${CDKTF_CLI_VERSION} \
-    --build-arg PRIMARY_DOMAIN_NAME=${PRIMARY_DOMAIN_NAME} \
-    --cache-from ${CI_REGISTRY_IMAGE}:latest \
-    --cache-from ${CI_REGISTRY_IMAGE}:${VERSION} \
-    --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}:latest \
-    --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}:${VERSION} \
-    --push \
-    --tag ${CI_REGISTRY_IMAGE}:latest \
-    --tag ${CI_REGISTRY_IMAGE}:${VERSION} \
-    .
+# if [ -z ${IMAGE_NAME+x} ]; then
+#   docker buildx build \
+#     --build-arg CDKTF_CLI_VERSION=${CDKTF_CLI_VERSION} \
+#     --build-arg PRIMARY_DOMAIN_NAME=${PRIMARY_DOMAIN_NAME} \
+#     --cache-from ${CI_REGISTRY_IMAGE}:latest \
+#     --cache-from ${CI_REGISTRY_IMAGE}:${VERSION} \
+#     --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}:latest \
+#     --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}:${VERSION} \
+#     --push \
+#     --tag ${CI_REGISTRY_IMAGE}:latest \
+#     --tag ${CI_REGISTRY_IMAGE}:${VERSION} \
+#     .
 
-elif [ ${IMAGE_NAME} = "cicd" ]; then
-  docker buildx build \
-    --build-arg CDKTF_CLI_VERSION=${CDKTF_CLI_VERSION} \
-    --build-arg PRIMARY_DOMAIN_NAME=${PRIMARY_DOMAIN_NAME} \
-    --cache-from ${CI_REGISTRY_IMAGE}:latest \
-    --cache-from ${CI_REGISTRY_IMAGE}:${VERSION} \
-    --cache-from ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
-    --cache-from ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:${VERSION} \
-    --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
-    --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:${VERSION} \
-    --file Dockerfile.cicd \
-    --push \
-    --tag ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
-    --tag ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:${VERSION} \
-    .
+# elif [ ${IMAGE_NAME} = "cicd" ]; then
+#   docker buildx build \
+#     --build-arg CDKTF_CLI_VERSION=${CDKTF_CLI_VERSION} \
+#     --build-arg PRIMARY_DOMAIN_NAME=${PRIMARY_DOMAIN_NAME} \
+#     --cache-from ${CI_REGISTRY_IMAGE}:latest \
+#     --cache-from ${CI_REGISTRY_IMAGE}:${VERSION} \
+#     --cache-from ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
+#     --cache-from ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:${VERSION} \
+#     --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
+#     --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:${VERSION} \
+#     --file Dockerfile.cicd \
+#     --push \
+#     --tag ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
+#     --tag ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:${VERSION} \
+#     .
 
-elif [ ${IMAGE_NAME} = "postgresql-repmgr" ]; then
-  docker buildx build \
-    --cache-from ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
-    --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
-    --file src/timestep/infra/stacks/kubernetes_config/postgresql_repmgr.Dockerfile \
-    --push \
-    --tag ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
-    src/timestep/infra/stacks/kubernetes_config
+# elif [ ${IMAGE_NAME} = "postgresql-repmgr" ]; then
+#   docker buildx build \
+#     --cache-from ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
+#     --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
+#     --file src/timestep/infra/stacks/kubernetes_config/postgresql_repmgr.Dockerfile \
+#     --push \
+#     --tag ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
+#     src/timestep/infra/stacks/kubernetes_config
 
-else
-  docker buildx build \
-    --build-arg CDKTF_CLI_VERSION=${CDKTF_CLI_VERSION} \
-    --build-arg PRIMARY_DOMAIN_NAME=${PRIMARY_DOMAIN_NAME} \
-    --cache-from ${CI_REGISTRY_IMAGE}:latest \
-    --cache-from ${CI_REGISTRY_IMAGE}:${VERSION} \
-    --cache-from ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
-    --cache-from ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:${VERSION} \
-    --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
-    --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:${VERSION} \
-    --push \
-    --tag ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
-    --tag ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:${VERSION} \
-    src/timestep/services/${IMAGE_NAME}
+# else
+#   docker buildx build \
+#     --build-arg CDKTF_CLI_VERSION=${CDKTF_CLI_VERSION} \
+#     --build-arg PRIMARY_DOMAIN_NAME=${PRIMARY_DOMAIN_NAME} \
+#     --cache-from ${CI_REGISTRY_IMAGE}:latest \
+#     --cache-from ${CI_REGISTRY_IMAGE}:${VERSION} \
+#     --cache-from ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
+#     --cache-from ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:${VERSION} \
+#     --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
+#     --cache-to type=inline,ref=${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:${VERSION} \
+#     --push \
+#     --tag ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:latest \
+#     --tag ${CI_REGISTRY_IMAGE}/${IMAGE_NAME}:${VERSION} \
+#     src/timestep/services/${IMAGE_NAME}
 
-fi
+# fi
