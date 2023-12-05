c2f40e2ebc2503bee6aaa2ba1997766260ef8e7f
Michael Schock <m@mjschock.com>
2023-12-04 19:07:58 -0800

    testing

--- src/timestep/infra/cicd/build.sh
+++ src/timestep/infra/cicd/build.sh
@@ -16,14 +16,19 @@ set -x # echo on
 json_secrets=$(cat secrets.json)
 echo "json_secrets: ${json_secrets}"
 
-# Read vars.json to json_vars
-json_vars=$(cat vars.json)
-echo "json_vars: ${json_vars}"
+json_secrets2=$(cat secrets2.json)
+echo "json_secrets2: ${json_secrets2}"
 
-# For each key in json_secrets, export the key/value pair
-for key in $(echo ${json_secrets} | jq -r "keys[]" ); do
-  export $key=$(echo ${json_secrets} | jq -r ".[\"$key\"]")
-done
+# Read vars.json to json_vars
+# json_vars=$(cat vars.json)
+# echo "json_vars: ${json_vars}"
+
+# # For each key in json_secrets, export the key/value pair
+# for key in $(echo ${json_secrets} | jq -r "keys[]" ); do
+#   export $key=$(echo ${json_secrets} | jq -r ".[\"$key\"]")
+# done
+
+
 
 # mkdir -p secrets
 # Use jq to parse the json_secrets and write to secrets
