f603ab8f7c43a985c347b4d38218164f4a4997a6
Michael Schock <m@mjschock.com>
2023-12-06 14:42:00 -0800

    fix: f

--- src/timestep/infra/cicd/build.sh
+++ src/timestep/infra/cicd/build.sh
@@ -1,57 +1,12 @@
 #!/usr/bin/env bash
 set -e # exit on first error
-# set -x # echo on
-
-# If direnv is not installed,
-# then install it
-# and add the hook to the shell
-# and allow the current directory
-# and rerun this script
-# if ! command -v direnv &> /dev/null
-# then
-#   echo "direnv could not be found"
-#   echo "Installing direnv"
-#   curl -sfL https://direnv.net/install.sh | bash
-#   eval "$(direnv hook bash)"
-#   direnv allow .
-#   # Sleep for a short time to allow the shell to settle
-#   sleep 1
-#   # Source this script to apply changes to the current shell session
-#   source "$0" "$@"
-#   exit  # Ensure the script exits after sourcing
-# else
-#   echo "direnv is installed"
-# fi
 
 curl -sfL https://direnv.net/install.sh | bash
-# eval "$(direnv hook bash)"
-# direnv allow .
 
 eval "$(direnv dotenv bash .dot.env)"
 eval "$(direnv dotenv bash .env)"
 
-# cat .dot.env | grep ^CDKTF_CLI_VERSION | cut -d '=' -f2
-# echo "CDKTF_CLI_VERSION=${CDKTF_CLI_VERSION}"
-
-# cat .env | grep ^CLOUD_INSTANCE_PROVIDER | cut -d '=' -f2
-# echo "CLOUD_INSTANCE_PROVIDER=${CLOUD_INSTANCE_PROVIDER}"
-
-echo "CI_REGISTRY_IMAGE=${CI_REGISTRY_IMAGE}"
-echo "DOCKER_REGISTRY_USERNAME=${DOCKER_REGISTRY_USERNAME}"
-echo "DOCKER_REGISTRY_PASSWORD=${DOCKER_REGISTRY_PASSWORD}"
-echo "DOCKER_REGISTRY_SERVER=${DOCKER_REGISTRY_SERVER}"
-echo "IMAGE_NAME=${IMAGE_NAME}"
-echo "PRIMARY_DOMAIN_NAME=${PRIMARY_DOMAIN_NAME}"
-echo "SHELL=${SHELL}"
-echo "VERSION=${VERSION}"
-
-# ls -al secrets
-
-# TODO: Load these using direnv w/ dotenv
-# CDKTF_CLI_VERSION=$(cat .env | grep ^CDKTF_CLI_VERSION | cut -d '=' -f2)
-# VERSION=$(cat .env | grep ^VERSION | cut -d '=' -f2)
-
-# docker login -u ${DOCKER_REGISTRY_USERNAME} -p ${DOCKER_REGISTRY_PASSWORD} ${DOCKER_REGISTRY_SERVER}
+docker login -u ${DOCKER_REGISTRY_USERNAME} -p $(cat ./secrets/docker_registry_password) ${DOCKER_REGISTRY_SERVER}
 
 # if [ -z ${IMAGE_NAME+x} ]; then
 #   docker buildx build \
