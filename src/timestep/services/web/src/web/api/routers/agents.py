a6dfaeda7d81857a6fa679624acf25d15a5c99dc
Michael Schock <m@mjschock.com>
2023-12-16 08:33:47 -0800

    fix: f

--- src/timestep/services/web/src/web/api/routers/agents.py
+++ src/timestep/services/web/src/web/api/routers/agents.py
@@ -1,11 +1,15 @@
 import logging
+from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from web.services.agents import AgentsService, init_agents_service
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 agents_router = APIRouter(tags=["agents"])
+security = HTTPBearer()
 
 
 @agents_router.on_event("startup")
@@ -16,10 +20,16 @@ async def startup():
 
 
 @agents_router.post("")
-async def create_agent():
+async def create_agent(
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)],
+):
     logger.debug("Creating agent")
+    logger.debug(f"credentials: {credentials}")
 
     # agent = await agents_router.state.agents_service.create_agent()
-    agent = {"message": "Agent created"}
+    # agent = {"message": "Agent created"}
+
+    agents_service: AgentsService = await init_agents_service()
+    agent = await agents_service.create_agent()
 
     return agent
