6075e7113a742bc5acc2b91311027f02d6c322db
Michael Schock <m@mjschock.com>
2024-01-19 18:18:14 -0800

    fix: f

--- src/timestep/infra/cicd/deploy.sh
+++ src/timestep/infra/cicd/deploy.sh
@@ -8,9 +8,18 @@ eval "$(direnv dotenv bash .env)"
 
 docker login -u ${DOCKER_REGISTRY_USERNAME} -p $(cat ./secrets/docker_registry_password) ${DOCKER_REGISTRY_SERVER}
 
+# docker run \
+#  --env-file .dot.env \
+#  --env-file .env \
+#  --user $(id -u):$(id -g) \
+#  --volume $(pwd)/secrets:/home/ubuntu/secrets:rw \
+#  ${CI_REGISTRY_IMAGE}/cicd:latest poetry run cdktf deploy --auto-approve ${PRIMARY_DOMAIN_NAME}.k3s_cluster ${PRIMARY_DOMAIN_NAME}.kubernetes_config ${PRIMARY_DOMAIN_NAME}.platform
+
+echo "id -u: $(id -u)"
+echo "id -g: $(id -g)"
+
 docker run \
  --env-file .dot.env \
  --env-file .env \
- --user $(id -u):$(id -g) \
  --volume $(pwd)/secrets:/home/ubuntu/secrets:rw \
  ${CI_REGISTRY_IMAGE}/cicd:latest poetry run cdktf deploy --auto-approve ${PRIMARY_DOMAIN_NAME}.k3s_cluster ${PRIMARY_DOMAIN_NAME}.kubernetes_config ${PRIMARY_DOMAIN_NAME}.platform
