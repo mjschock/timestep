aba4953ec34f16f9904108640435174397a87b70
Michael James Schock <m@mjschock.com>
2024-05-26 11:20:02 -0700

    fix: f

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,13 @@ direnv allow .
 eval "$(anyenv init -)"
 source /home/ubuntu/.venv/bin/activate
 
-sudo chown -R ubuntu:ubuntu /home/ubuntu/secrets
+# sudo chown -R ubuntu:ubuntu /home/ubuntu/secrets
+
+echo "=== Running docker-entrypoint.sh ==="
+echo "=== Running as $(whoami) ==="
+echo "=== Running with $(id -u):$(id -g) ==="
+echo "=== Running in $(pwd) ==="
+echo "=== Running with secrets ==="
+ls -al /home/ubuntu/secrets
 
 exec "$@"
