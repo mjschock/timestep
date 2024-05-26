8676f8814f8594c22a7d59fe5acc952db8da9f8d
Michael James Schock <m@mjschock.com>
2024-05-26 13:32:48 -0700

    fix: f

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,20 +5,12 @@ eval "$(direnv hook bash)"
 direnv allow .
 
 eval "$(anyenv init -)"
-source /home/ubuntu/.venv/bin/activate
+# source /home/ubuntu/.venv/bin/activate
+source .venv/bin/activate
 
-echo "=== Running docker-entrypoint.sh ==="
-echo "=== Running as $(whoami) ==="
-echo "=== Running with $(id -u):$(id -g) ==="
-echo "=== Running in $(pwd) ==="
-
-echo "=== secrets before ==="
-ls -al /home/ubuntu/secrets
-
-# chown -R ubuntu:ubuntu /home/ubuntu/secrets
-sudo chown -R ubuntu:ubuntu /home/ubuntu/secrets
-
-echo "=== secrets after ==="
-ls -al /home/ubuntu/secrets
+# if /home/ubuntu/secrets exists, change ownership to ubuntu
+if [ -d /home/ubuntu/secrets ]; then
+  sudo chown -R ubuntu:ubuntu /home/ubuntu/secrets
+fi
 
 exec "$@"
