6e30060bc717fc68835d8c73f6650f232cdfab2c
Michael James Schock <m@mjschock.com>
2024-06-11 11:39:27 -0700

    fix: f
    Former-commit-id: 52efd0b752caf2c029168fa80b9e2940e39eb43c

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,24 +11,26 @@ if [ -d .venv ]; then
   source .venv/bin/activate
 fi
 
-# if /home/ubuntu/.ssh exists, change ownership to id -u:$(id -g)
-if [ -d /home/ubuntu/.ssh ]; then
-  # sudo chown -R ubuntu:ubuntu /home/ubuntu/.ssh
-  sudo chown -R $(id -u):$(id -g) /home/ubuntu/.ssh
-fi
+# # if /home/ubuntu/.ssh exists, change ownership to id -u:$(id -g)
+# if [ -d /home/ubuntu/.ssh ]; then
+#   # sudo chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+#   sudo chown -R $(id -u):$(id -g) /home/ubuntu/.ssh
+# fi
 
-# if /home/ubuntu/.sky exists, change ownership to id -u:$(id -g)
-if [ -d /home/ubuntu/.sky ]; then
-  # sudo chown -R ubuntu:ubuntu /home/ubuntu/.sky
-  sudo chown -R $(id -u):$(id -g) /home/ubuntu/.sky
-fi
+# # if /home/ubuntu/.sky exists, change ownership to id -u:$(id -g)
+# if [ -d /home/ubuntu/.sky ]; then
+#   # sudo chown -R ubuntu:ubuntu /home/ubuntu/.sky
+#   sudo chown -R $(id -u):$(id -g) /home/ubuntu/.sky
+# fi
 
-# if /home/ubuntu/secrets exists, change ownership to id -u:$(id -g)
-if [ -d /home/ubuntu/secrets ]; then
-  # sudo chown -R ubuntu:ubuntu /home/ubuntu/secrets
-  sudo chown -R $(id -u):$(id -g) /home/ubuntu/secrets
-fi
+# # if /home/ubuntu/secrets exists, change ownership to id -u:$(id -g)
+# if [ -d /home/ubuntu/secrets ]; then
+#   # sudo chown -R ubuntu:ubuntu /home/ubuntu/secrets
+#   sudo chown -R $(id -u):$(id -g) /home/ubuntu/secrets
+# fi
 
 # TODO: just chown -R $(id -u):$(id -g) /home/ubuntu?
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu
 
 exec "$@"
