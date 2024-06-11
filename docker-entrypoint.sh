4ea9f223480750378146a223c3b7fd90dc559b38
Michael James Schock <m@mjschock.com>
2024-06-11 09:03:20 -0700

    fix: f
    Former-commit-id: 491c6b63b26a8f83e48ad075e88f977b132534b4

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,21 +11,18 @@ if [ -d .venv ]; then
   source .venv/bin/activate
 fi
 
-# if /home/ubuntu/.ssh exists, change ownership to ubuntu
 # if /home/ubuntu/.ssh exists, change ownership to id -u:$(id -g)
 if [ -d /home/ubuntu/.ssh ]; then
   # sudo chown -R ubuntu:ubuntu /home/ubuntu/.ssh
   sudo chown -R $(id -u):$(id -g) /home/ubuntu/.ssh
 fi
 
-# if /home/ubuntu/.sky exists, change ownership to ubuntu
 # if /home/ubuntu/.sky exists, change ownership to id -u:$(id -g)
 if [ -d /home/ubuntu/.sky ]; then
   # sudo chown -R ubuntu:ubuntu /home/ubuntu/.sky
   sudo chown -R $(id -u):$(id -g) /home/ubuntu/.sky
 fi
 
-# if /home/ubuntu/secrets exists, change ownership to ubuntu
 # if /home/ubuntu/secrets exists, change ownership to id -u:$(id -g)
 if [ -d /home/ubuntu/secrets ]; then
   # sudo chown -R ubuntu:ubuntu /home/ubuntu/secrets
