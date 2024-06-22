1306bcd74cf8d99521a5763885f30a4381fb62f8
Michael James Schock <m@mjschock.com>
2024-06-22 11:36:48 -0700

    clear

--- src/timestep/agents/transformers/models/model/configuration_model.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from transformers import PretrainedConfig
-
-
-class ModelConfig(PretrainedConfig):  # type: ignore[misc]
-    model_type = "model"
--- src/timestep/agents/transformers/models/model/modeling_model.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from transformers import PreTrainedModel
-
-from timestep.agents.transformers.models.model.configuration_model import ModelConfig
-
-
-class ModelModel(PreTrainedModel):  # type: ignore[misc]
-    config_class = ModelConfig
