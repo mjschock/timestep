f7d8c002f1c57b5a9e312a059e804c13c235d698
Michael James Schock <m@mjschock.com>
2024-06-22 10:40:48 -0700

    clear
    Former-commit-id: 4650a02a541ea38cb7b9b529d6aa4295671578c7

--- src/timestep/infra/stacks/kubernetes_config/kubernetes_dashboard/scripts/get_credentials.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e # exit on first error
-
-echo
-kubectl get --namespace kubernetes-dashboard secret admin-user -o go-template='{{.data.token | base64decode}}'
-echo
--- tests/locustfile.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task
-
-
-class HelloWorldUser(HttpUser):
-    @task
-    def hello_world(self):
-        self.client.get("/ping", verify=False)
-        self.client.get("/api/version", verify=False)
