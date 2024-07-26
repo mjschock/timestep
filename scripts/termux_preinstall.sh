e9f08ee321df7222065e20d46484235c0d06982a
Michael James Schock <m@mjschock.com>
2024-07-26 17:31:15 -0400

    f

--- scripts/termux_preinstall.sh
+++ scripts/termux_preinstall.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -ex
 
-yes | pkg upgrade
+pkg upgrade
 
-pkg install binutils-is-llvm build-essential cmake libopenblas libandroid-execinfo libzmq ninja patchelf python
+pkg install binutils-is-llvm build-essential cmake libopenblas libandroid-execinfo libzmq ninja patchelf python python-pyarrow python-scipy tur-repo
 
-pip3 install cython meson-python packaging pyproject_metadata ruamel.yaml.clib setuptools versioneer wheel
+pip3 install cython meson-python packaging pyproject_metadata ruamel.yaml.clib setuptools scikit-build-core versioneer wheel
 
-MATHLIB=m LDFLAGS="-lpython3.11" pip3 install --no-build-isolation --no-cache-dir numpy==1.26.4
+MATHLIB=m LDFLAGS="-lpython3.11" pip3 install --no-build-isolation --no-cache-dir numpy==1.26.4 # 2.0.1
 
-LDFLAGS="-lpython3.11" pip3 install --no-build-isolation --no-cache-dir pandas
+LDFLAGS="-lpython3.11" pip3 install --no-build-isolation --no-cache-dir pandas==2.2.2
 
-#pip install scikit-build-core
+# LDFLAGS="-lpython3.11" pip3 install --upgrade --no-build-isolation --no-cache-dir --use-deprecated=legacy-resolver pyarrow==17.0.0
+
+# LDFLAGS="-lpython3.11" pip3 install --no-build-isolation --no-cache-dir scipy==1.14.0
