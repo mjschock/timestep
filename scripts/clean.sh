36c4d531fe6699200e7fe537fcba8e950737f612
Michael James Schock <m@mjschock.com>
2024-07-29 20:07:27 -0700

    f
    Former-commit-id: 31013f80f0374e2b44c767fdeeab7d4593980fb7

---
+++ scripts/clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -ex
+
+rm -rf .venv
+python -m venv .venv
--- scripts/termux_preinstall.sh
+++ scripts/termux_preinstall.sh
@@ -3,9 +3,9 @@ set -ex
 
 pkg upgrade
 
-pkg install binutils-is-llvm build-essential cmake libopenblas libandroid-execinfo libzmq ninja patchelf python python-pyarrow python-scipy tur-repo
+pkg install  binutils-is-llvm build-essential cmake clang libopenblas libandroid-execinfo libzmq ninja patchelf python python-pip python-pyarrow python-scipy tur-repo
 
-pip3 install cython meson-python packaging pyproject_metadata ruamel.yaml.clib setuptools scikit-build-core versioneer wheel
+pip3 install cython meson-python packaging pyproject_metadata ruamel.yaml.clib setuptools setuptools_rust scikit-build-core versioneer wheel
 
 MATHLIB=m LDFLAGS="-lpython3.11" pip3 install --no-build-isolation --no-cache-dir numpy==1.26.4 # 2.0.1
 
