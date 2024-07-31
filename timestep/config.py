f5125fb9c7a8575297bf327f00b3b5ec12de4155
Michael James Schock <m@mjschock.com>
2024-07-31 08:15:36 -0700

    f
    Former-commit-id: 8ec4aae25233bf4beb2f16dd51ec0e80d1e9bdc3

---
+++ timestep/config.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from pydantic import Field, SecretStr
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    openai_api_key: SecretStr = Field(default=None)
+    poetry_pypi_token_testpypi: SecretStr = Field(default=None)
+    poetry_virtualenvs_create: bool = Field()
+    poetry_virtualenvs_in_project: bool = Field()
+    prefect_api_key: Optional[SecretStr] = Field(default=None)
+    prefect_api_url: str = Field(
+        default="http://prefect-server.default.svc.cluster.local:4200/api"
+    )
+    pyenv_version: str = Field()
+    version: str = Field()
+
+    class Config:
+        env_file = (".env")
+        env_file_encoding = "utf-8"
+        secrets_dir = "./secrets"
