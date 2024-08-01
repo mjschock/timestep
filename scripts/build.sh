87c614062dd7261d17c22fe109a20a4d84f96eda
Michael James Schock <m@mjschock.com>
2024-07-31 17:23:14 -0700

    12.0.16
    Former-commit-id: d95e88b8a1572802f7ff4bc68415174e33de9d0f

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,3 @@ curl -sfL https://direnv.net/install.sh | bash
 eval "$(direnv dotenv bash .env)"
 
 make
-
-poetry config repositories.testpypi $POETRY_REPOSITORIES_TESTPYPI_URL
-poetry publish --build --no-interaction --password=$(cat ./secrets/poetry_pypi_token_testpypi) --repository=testpypi --username=__token__
