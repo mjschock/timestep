f8f829316163b7f86768917db1aa0e6c69ad1e0f
Michael James Schock <m@mjschock.com>
2024-07-31 17:46:42 -0700

    12.0.18

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,12 @@ curl -sfL https://direnv.net/install.sh | bash
 
 eval "$(direnv dotenv bash .env)"
 
-make
+echo 'TODO: Move the `make pre-commit` commands to a pre-commit hook'
+git submodule update --init --recursive
+poetry config repositories.testpypi $POETRY_REPOSITORIES_TESTPYPI_URL
+poetry install
+poetry run black timestep
+poetry run isort timestep # https://pycqa.github.io/isort/docs/configuration/black_compatibility.html#integration-with-pre-commit
+poetry run pytest
+poetry run toml-sort -ai pyproject.toml
+poetry run typer timestep.main utils docs --name timestep --output README.md --title "Timestep AI"
